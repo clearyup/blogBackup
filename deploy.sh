1a80572dc44c8ce2b7e49fecb5a7e5a1699290a2
clearupclore <2010059016@qq.com>
2021-04-12 19:48:33 +0800

    update site 2021年04月12日 19:48:33

--- deploy.sh
+++ deploy.sh
@@ -3,34 +3,34 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-# Build the project.
+# 生成静态页面(渲染草稿)
 hugo -D 
 
 git add .
 
-# Commit changes.
-msg="rebuilding site $(date)"
+# 根据日期commmit
+msg="update site $(date)"
 if [ -n "$*" ]; then
     msg="$*"
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
+# Push 到远程仓库
 git push origin master
 
 
-# Go To Public folder
+# 到public 仓库
 cd public
 
-# Add changes to git.
+
 git add .
 
-# Commit changes.
+# 根据日期commit
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
     msg="$*"
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
+# Push 
 git push origin master
