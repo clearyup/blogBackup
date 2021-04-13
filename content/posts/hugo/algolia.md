187f468bb3ef9f2fa0f582b231b74fd1f5b3bac9
clearupclore <2010059016@qq.com>
2021-04-13 20:46:23 +0800

    update site 2021年04月13日 20:46:23

---
+++ content/posts/hugo/algolia.md
@@ -0,0 +1,18 @@
+---
+title: "Algolia"
+date: 2021-04-13T20:35:14+08:00
+draft: false
+tags: ["algolia"]
+categories: ["hugo"]
+lightgallery: true
+---
+
+# algolia配置
+
+```node
+npm run algolia 
+call npm run algolia 
+```
+
+
+
--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,6 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # 生成静态页面(渲染草稿)
 hugo -D 
 
-
-
 git add .
 
 # 根据日期commmit
@@ -37,3 +35,6 @@ git commit -m "$msg"
 # Push 
 git push -f origin master
 
+cd ..
+
+npm run algolia
